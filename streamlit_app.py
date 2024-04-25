a19aea5306257a7d00129cf6026cd377be9581b1
Ayush Acharya <ayushach007@gmail.com>
2024-04-25 08:40:10 +0545

    $ git commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,12 +26,25 @@ model = st.sidebar.selectbox('Select a model', list(models.keys()))
 # Create a text box for the user to enter the text
 text = st.text_area('Enter the text here:')
 
+
 # Create a button to predict the sentiment
 if st.button('Predict'):
     model = models[model]
     prediction = model.predict([text])
     prediction = 'Positive' if prediction[0] == 1 else 'Negative'
     st.write(f'The predicted sentiment is: {prediction}')
+
+# add a word cloud generator
+st.sidebar.write('Word Cloud Generator')
+st.sidebar.write('This is a simple word cloud generator that generates a word cloud from the given text. The word cloud shows the most common words in the text.')
+st.sidebar.write('Please enter a text in the text box below and click the "Generate" button to see the word cloud.')
+
+if st.sidebar.button('Generate'):
+    text = TextPreprocessor().transform(text)
+    text = TextTokenizer().transform(text)
+    text = ' '.join(text)
+    st.write('The word cloud is generated below:')
+    st.image(f'word_cloud.png', use_column_width=True)
 
 
         
