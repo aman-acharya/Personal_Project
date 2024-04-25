604c8a7ed057a4b8552c6764dfa95089578a73c5
Ayush Acharya <ayushach007@gmail.com>
2024-04-25 08:35:22 +0545

    $ git commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ models = {'Logistic Regression': Logistic, 'Support Vector Machine': SVM, 'Rando
 # Create a title for the app
 st.title('Sentiment Analysis App')
 
-st.sidebar(title='Sentiment Analysis App')
+st.sidebar.write(title='Sentiment Analysis App')
 st.sidebar.write('This is a simple sentiment analysis app that uses four different models to predict the sentiment of a given text. The models are trained on the IMDB dataset and can predict whether a given text is positive or negative. The models are Logistic Regression, Support Vector Machine, Random Forest, and Naive Bayes.')
 st.sidebar.write('Please enter a text in the text box below and click the "Predict" button to see the predictions of the models.')
 
@@ -28,26 +28,13 @@ text = st.text_area('Enter the text here:')
 
 # Create a button to predict the sentiment
 if st.button('Predict'):
-    if text:
-        # Load the text preprocessor and tokenizer
-        preprocessor = TextPreprocessor()
-        tokenizer = TextTokenizer()
+    model = models[model]
+    prediction = model.predict([text])
+    prediction = 'Positive' if prediction[0] == 1 else 'Negative'
+    st.write(f'The predicted sentiment is: {prediction}')
+    
 
-        # Preprocess the text
-        text = preprocessor.transform([text])
-
-        # Tokenize the text
-        text = tokenizer.transform(text)
-
-        # Predict the sentiment
-        prediction = models[model].predict(text)
-
-        # Display the prediction
-        st.write(f'The sentiment of the text is: {prediction[0]}')
-    else:
-        st.write('Please enter a text in the text box above to predict the sentiment.')
-
-
+        
 
 
 
